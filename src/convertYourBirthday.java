6b7755a83af81f241ca08aebc9e6d5d3ed957d07
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-09-26 13:43:29 -0400

    Finished.

--- src/convertYourBirthday.java
+++ src/convertYourBirthday.java
@@ -1,2 +1,23 @@
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class convertYourBirthday {
+
+
+    public static void main(String[] args) {
+        LocalDateTime birthday = LocalDateTime.of(3333, 3, 3, 13, 33, 3);
+        ZoneId USA = ZoneId.of("America/New_York");
+        System.out.println(birthdayInParis(birthday, USA));
+    }
+
+    public static ZonedDateTime birthdayInParis(LocalDateTime date, ZoneId timeZone){
+        ZonedDateTime normalDate = ZonedDateTime.of(date, timeZone);
+        ZoneId paris = ZoneId.of("Europe/Paris");
+
+        return(normalDate.withZoneSameInstant(paris));
+    }
 }
+
+
